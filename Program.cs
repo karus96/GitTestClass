d7077503fea0f9c0403d97653f97c9b52588734c
Fherreram <fiorellaherrera70@gmail.com>
2024-09-23 13:42:14 -0600

    Fiorella tarea 2 completa

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿/*
+    // Suma de dos numeros
+Console.WriteLine("Digite el numero 1");
+String read = Console.ReadLine();
+int numero1= int.Parse(read);
+Console.WriteLine("Digite el numero 2");
+read = Console.ReadLine();
+int numero2 = int.Parse(read);
+Console.WriteLine("La suma es");
+Console.WriteLine("numero1+numero2");
+*/
+/*
+// Mayor de tres numeros
+int numero =1;
+int numero =2;
+int numero =3;
+
+    if (numero1 > numero2 && numero1 > numero3)
+    {
+    Console.WriteLine("El numero uno es mayor");
+} else if (numero2 > numero3)
+{
+    Console.WriteLine("El numero dos es mayor al numero tres");
+}
+else
+{
+    Console.WriteLine("El numero tres es menor");
+}
+*/
+// Si un numero es positivo,negativo o cero
+/*
+int numero1;
+ if(numero >0)
+{
+    Console.WriteLine("El numero es positivo");
+} else if (numero <0)
+{
+    Console.WriteLine("El numero es negrativo");
+}
+else
+{
+    Console.WriteLine("El numero es cero");
+}
+*/
+// Calcular el area de un circulo
+Console.WriteLine("Determine el area de la circunferencia");
+Console.WriteLine("El radio es la mitad de la circunferencia");
+Console.WriteLine("La formula para sacar el area es π*r*2");
+Console.WriteLine("El valor de π es 3,14");
+const double π = 3.14;
+Console.WriteLine(π);
+Console.WriteLine("Digite el valor de π");
+String read = Console.ReadLine();
+Console.WriteLine("Digite el radio de la circunferencia");
+String read2 = Console.ReadLine();
+int radio = int.Parse(read2);
+double elevadoalcuadraro =
+Math.Pow(radio,2);
+Console.WriteLine(string.Format("{0} elevado al cuadrado es {1}", radio, elevado al cuadrado)); 
+Console.WriteLine("el resultado es");
+int area = int.Parse(read2);
+Console.WriteLine(π * elevado al cuadrado); 
+
+// Determinar si un año es bisiesto
+/*
+ Console.WriteLine("Digite el año");
+int año = int.Parse(Console.ReadLine());
+if ((año % 4 == 0 && año % 100 != 0) || año % 400 == 0)
+{
+   Console.WriteLine("El año es bisiesto");
+}
+else
+{
+    Console.WriteLine("El año no es bisiesto");
+}
+*/
+//Semaforo
+/*
+  int semaforo = 2;
+
+    if(semaforo ==2)
+    {
+    Console.WriteLine("Puedo moverme");
+} else if (semaforo ==1)
+{
+    Console.WriteLine("Debo comenzar a deternerme");
+}
+else
+{
+    Console.WriteLine("Debo detenerme");
+}
+*/
